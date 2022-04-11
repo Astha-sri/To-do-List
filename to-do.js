70fc06d3018ff579c2a75a6ec7583b18aa14ec4d
Astha-sri <68340662+Astha-sri@users.noreply.github.com>
2022-04-11 11:37:23 +0530

    Add files via upload

---
+++ to-do.js
@@ -0,0 +1,25 @@
+document.querySelector('#add').onclick = function(){
+    if(document.querySelector('#task input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="newtask">
+                <span id="taskname">
+                    ${document.querySelector('#task input').value}
+                </span>
+                <button class="delete">
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
